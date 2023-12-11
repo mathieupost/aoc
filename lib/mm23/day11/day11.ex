964fab139c67c24da3be35f7effe407419f4fec5
Mathieu Post <mathieupost@gmail.com>
2023-12-11 12:53:27 +0100

    day 11 part 2

--- lib/mm23/day11/day11.ex
+++ lib/mm23/day11/day11.ex
@@ -63,4 +63,65 @@ defmodule MM23.Day11 do
   def transpose(matrix) do
     Enum.zip(matrix) |> Enum.map(&Tuple.to_list/1)
   end
+
+  @doc """
+  Part 2
+
+  ## Examples
+
+      iex> MM23.Day11.part2("lib/mm23/day11/example.txt", 2)
+      374
+
+      iex> MM23.Day11.part2("lib/mm23/day11/example.txt", 10)
+      1030
+
+      iex> MM23.Day11.part2("lib/mm23/day11/example.txt", 100)
+      8410
+
+      iex> MM23.Day11.part2("lib/mm23/day11/input.txt", 1000000)
+      649862989626
+
+  """
+  def part2(input, expand) do
+    File.read!(input)
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.graphemes/1)
+    |> replace_rows()
+    |> transpose()
+    |> replace_rows()
+    |> transpose()
+    |> find_galaxies(expand)
+    |> calculate_distances()
+    |> Enum.sum()
+  end
+
+  def find_galaxies(matrix, expand) do
+    {_, acc} =
+      Enum.reduce(matrix, {0, []}, fn row, {y, acc} ->
+        # Each cell with index as x
+        {x, acc} =
+          Enum.reduce(row, {0, acc}, fn
+            ".", {x, acc} -> {x + 1, acc}
+            "X", {x, acc} -> {x + expand, acc}
+            "#", {x, acc} -> {x + 1, acc ++ [{y, x}]}
+          end)
+
+        if x == expand * Enum.count(row) do
+          {y + expand, acc}
+        else
+          {y + 1, acc}
+        end
+      end)
+
+    acc
+  end
+
+  def replace_rows(matrix) do
+    Enum.reduce(matrix, [], fn row, acc ->
+      case Enum.all?(row, &(&1 != "#")) do
+        true -> acc ++ [Enum.map(row, fn _ -> "X" end)]
+        false -> acc ++ [row]
+      end
+    end)
+  end
 end
